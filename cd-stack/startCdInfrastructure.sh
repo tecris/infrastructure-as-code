3c3edd33918ce5271ecf8f5de5f0c4eb06e12094
Eugen Cristea <eugen@cristea.org>
2016-07-11 11:20:57 +0300

    xenial update

--- cd-stack/startCdInfrastructure.sh
+++ cd-stack/startCdInfrastructure.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
-export DOCKER_IP=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
+# ubuntu 16.04 xenial
+export DOCKER_IP=`ip -o -4 addr list wlp3s0 | awk '{print $4}' | cut -d/ -f1`
+
+# ubuntu 14.04
+# export DOCKER_IP=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
 
 docker-compose up -d
