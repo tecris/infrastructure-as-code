3d70a1dfd020c0f07b12f53f5f77f27734ae2f54
Eugen Cristea <eugen@cristea.org>
2017-08-06 16:26:42 +0200

    network interface

--- cd-stack/startCdInfrastructure.sh
+++ cd-stack/startCdInfrastructure.sh
@@ -3,8 +3,9 @@
 
 # ubuntu 16.04 xenial
 export DOCKER_IP=`ip -o -4 addr list wlp3s0 | awk '{print $4}' | cut -d/ -f1`
+# export DOCKER_IP=`ip -o -4 addr list enp0s3 | awk '{print $4}' | cut -d/ -f1`
 
 # ubuntu 14.04
 # export DOCKER_IP=`ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
 
-docker-compose up -d
+docker-compose up -d jenkins
